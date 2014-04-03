00dded51ae0b831008a1fb25cd95c9a9a02b6216
Stephen Sylvia <ssylvia@esri.com>
2014-04-03 15:31:59 -0400

    folder structure setup

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+  // Add loader for Grunt plugins
+  require("matchdep").filterDev(["grunt-*","assemble"]).forEach(grunt.loadNpmTasks);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    assemble: {
+      dev: {
+        options: {
+          layoutdir: 'src/app/templates/layouts',
+          layoutext: '.hbs',
+          data: ['data/**/*.{json,yml}'],
+          postprocess: require('pretty'),
+          production: false
+        },
+        expand: true,
+        flatten: true,
+        src: ['src/app/pages/**/*.hbs'],
+        dest: 'src/'
+      }
+    },
+
+    connect: {
+      dev: {
+        options:{
+          base: 'src/'
+        }
+      }
+    },
+
+    stylus: {
+      dev: {
+        options: {
+          compress: false,
+          linenos: true,
+          paths: ['src/app/stylesheets','src/bower_components/'],
+          use: [
+            require('nib')
+          ],
+          'include css': true,
+        },
+        files: {
+          'src/app/stylesheets/app.css': 'src/app/stylesheets/app.styl'
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      assemble: {
+        files: ['src/app/pages/**/*.hbs','src/app/templates/layouts/**/*.hbs','data/**/*.{json,yml}'],
+        tasks: ['assemble:dev'],
+      },
+      stylus: {
+        files: ['src/app/stylesheets/**/*.styl'],
+        tasks: ['stylus:dev']
+      }
+    }
+
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', [
+    'connect:dev',
+    'watch'
+  ]);
+
+};
